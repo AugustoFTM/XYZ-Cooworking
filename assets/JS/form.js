446ed97b72b9fae0b720bfa6c318019e847579b2
AugustoFTM <218685156+PlayBoy-JogaFacil@users.noreply.github.com>
2025-07-25 00:05:36 -0300

    1° commit

---
+++ assets/JS/form.js
@@ -0,0 +1,6 @@
+// Função que adiciona ou remove a classe 'rotate' no container
+// Isso ativa a rotação 3D entre o formulário de login e cadastro
+function toggleForm() {
+    const container = document.getElementById("container");
+    container.classList.toggle("rotate");
+}
